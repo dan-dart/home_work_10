ae2b8224f7a6c99a5844f8b3673847deb5b4a403
Eshmatov Daniir <eshdan0@gmail.com>
2024-02-25 21:06:12 +0600

    My second comit

---
+++ test/lesson_with_circles_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_with_circles_3/lesson_with_circles_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
